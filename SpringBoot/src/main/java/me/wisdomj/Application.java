78ecc1187068da6cfe9b48a58cddb51ea126e74a
schatzen <gomieti@naver.com>
2020-04-18 19:31:28 +0900

    .

--- SpringBoot/src/main/java/me/wisdomj/Application.java
+++ SpringBoot/src/main/java/me/wisdomj/Application.java
@@ -9,7 +9,6 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 public class Application {
 
     public static void main(String[] args) {
-        // SpringApplication.run(Application.class, args);
 
         // 빌더로 실행하기 (커스터 마이징 가능)
 /*        new SpringApplicationBuilder()
@@ -19,9 +18,16 @@ public class Application {
         // 인스턴스로 만들기
         SpringApplication app = new SpringApplication(Application.class);
 
-        //배너끄기
-        app.setBannerMode(Banner.Mode.OFF);
+        //SampleListener 수동 실행
+        //app.addListeners(new SampleListener());
+
         app.run(args);
+
+        //배너끄기
+        //app.setBannerMode(Banner.Mode.OFF);
+
+
+        //SpringApplication.run(Application.class, args);
     }
 
 }
