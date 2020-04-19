60cabc614bca7b97bb01a3472d6965f77d257ea9
schatzen <gomieti@naver.com>
2020-04-20 00:38:21 +0900

    .

---
+++ externalSetting01/src/main/java/me/wisdomj/springinit.java
@@ -0,0 +1,12 @@
+package me.wisdomj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class springinit {
+
+    public static void main(String[] args) {
+        SpringApplication.run(springinit.class, args);
+    }
+}
